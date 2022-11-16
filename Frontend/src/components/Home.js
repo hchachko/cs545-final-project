16c0167cb06e3d9a2fbd20fc0e08dd9f859845f5
Harrison Chachko <hchachko@gmail.com>
2022-11-15 19:48:01 -0500

    Implemented search functionality

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -3,9 +3,12 @@ import './App.css';
 function Home () {
     return (
         <div>
-           <h1>
-            Home
-           </h1>
+           <div className='card'>
+                    <div className='card-body'>
+                        <h2 className='card-title'> What's our site about? </h2>
+                        <p>To view parks in the Hoboken area</p>
+                    </div>
+            </div>
         </div>
     );
 };
--- Frontend/src/components/Parks.js
+++ Frontend/src/components/Parks.js
@@ -6,7 +6,12 @@ import axios from 'axios';
 import {Link} from 'react-router-dom';
 function Home () {
     const [parksData, setParksData] = useState(undefined);
+    const [formData, setFormData] = useState({searchTerm: ''});
     let parks;
+    const handleChange = (e) => {
+        setFormData((prev) => ({...prev, [e.target.name]: e.target.value.toLowerCase()}));
+        console.log(formData);
+    };
     useEffect(() => {
         console.log("render");
         async function fetchData() {
@@ -20,10 +25,15 @@ function Home () {
         fetchData();
     }, []);
     parks = parksData && parksData.map((park) => {
-        console.log(park);
+        if (formData.searchTerm !== "") {
+            if (!(park.parkName.toLowerCase().includes(formData.searchTerm))) {
+                return(<div></div>);
+            }
+        } 
         let parkUrl = "/parks/"+park._id;
         return (
             <div className='parkCard' key = {park._id}>
+                
                  <Link to={parkUrl}>
                     <div className='card-body'>
                         <h4 className='card-title'>{park.parkName}</h4>
@@ -37,12 +47,27 @@ function Home () {
         <div>
             <Grid container spacing={1}>
             <Grid item xs={4} key="Parks">
-                <div className='parksCard'>
-                    <div className='card-body'>
-                        <h3>List of Hoboken Parks:</h3>
-                        {parks}
-                    </div>
+                <div className='searchCard'>
+                <br/>
+                <h3>List of Parks:</h3>
+                <div >
+                    <label>
+                    Filter by name:
+                    </label>
+                    <br/>
+                    <input
+                        onChange={(e) => handleChange(e)}
+                        id = 'searchTerm'
+                        name = 'searchTerm'
+                        placeholder = 'Search Parks...'
+                    />
                 </div>
+                </div>
+                    <div className='parksCard'>
+                        <div className='card-body'>
+                            {parks}
+                        </div>
+                    </div>
             </Grid>
             <Grid item xs={8} key="Hoboken_Map">
                 <div className='mapCard'>
