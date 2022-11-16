1fd8c4afa0e702a4b0d537d18ae2bd76a092c9f6
Matthew Viafora <55547270+Matthew-Viafora@users.noreply.github.com>
2022-11-16 16:33:14 -0500

    Update Account.js

--- Client/src/components/Account.js
+++ Client/src/components/Account.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles({
     border: "1px solid black",
     boxShadow: "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);",
     backgroundColor: "white",
+    margin: "2.5%",
   },
   grid: {
     flexGrow: 1,
